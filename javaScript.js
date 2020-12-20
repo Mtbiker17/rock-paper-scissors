3c4868e70fa2f6c2f92693136e6ad87e7485f12c
Devin <ospreywebservices@gmail.com>
2020-12-20 07:29:31 -0500

    added reset screen(end of game)

--- javaScript.js
+++ javaScript.js
@@ -5,25 +5,21 @@ let computerScore = 0;
 let computerScoreDisplay = document.querySelector("#computerScoreDisplay");
 let playerScoreDisplay = document.querySelector('#playerScoreDisplay');
 let resultNotification = document.querySelector('#displayResults');
+let endGameDisplay = document.querySelector('#main');
+let mainScreen = document.querySelector('#newMain');
+let gameContainer = document.querySelector('#game-container');
 
-
-
-function changeScore(){
-    computerScoreDisplay.textContent = `${computerScore}`;
-    playerScoreDisplay.textContent = `${playerScore}`;
-    resultNotification.textContent = `${resultString}`;
-}
-
+//Loop through each button and perform created functions to manipulate DOM
 buttons.forEach((button) => {
     button.addEventListener('click', () => {
         let playerSelection = (button.id);
         playRound(playerSelection, computerPlay());
         changeScore();
+        checkScore();
     });
 });
 
-
-//Create a function that has the computer randomly select rock, paper or scissors;
+//function that has the computer randomly select rock, paper or scissors;
 function computerPlay(){
     const number = (Math.random() * 3);
     if (number <= 1){
@@ -33,9 +29,7 @@ function computerPlay(){
     } else return 'paper';
 }
 
-//Create a function that plays one round of rock paper scissors;
-//Determine a scoring system;
-
+//function that plays one round of rock paper scissors; increment player and computer score
 function playRound(playerSelection, computerSelection){
     if (playerSelection == 'rock' && computerSelection == 'scissors'){
         playerScore++;
@@ -57,5 +51,37 @@ function playRound(playerSelection, computerSelection){
         resultString = 'You lose! Computer chose rock, rock beats scissors!';
     } else if (playerSelection === computerSelection){
         resultString = 'Draw! You both selected the same, try again!';
+    }
+}
+
+//Changes the score and result text in the DOM
+function changeScore(){
+    computerScoreDisplay.textContent = `${computerScore}`;
+    playerScoreDisplay.textContent = `${playerScore}`;
+    resultNotification.textContent = `${resultString}`;
+}
+
+//develop a screen reset upon either computer or player reaching a score of 5
+function checkScore(){
+    const endScreen = document.createElement('p');
+    endScreen.classList.add('endGame');
+    const resetButton = document.createElement('button');
+    resetButton.classList.add('button2');
+    if(playerScore === 5){
+        endScreen.textContent = 'Congratulations! You are the superior being.';
+        mainScreen.replaceWith(endScreen);
+        resetButton.textContent = "Try again?"
+        gameContainer.replaceWith(resetButton);
+        resetButton.addEventListener('click', () =>{
+            location.reload();
+        });
+    } else if (computerScore === 5){
+        endScreen.textContent = `You lose! Why don't you 0111111011011!`;
+        mainScreen.replaceWith(endScreen);
+        resetButton.textContent = "Try again?"
+        gameContainer.replaceWith(resetButton);
+        resetButton.addEventListener('click', () =>{
+            location.reload();
+        });
     }
 }
